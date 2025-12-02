15fa34a2753354149eb6d875b453ac9ccd9bc1c1
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2025-12-02 09:32:45 +0600

    Set up practices

---
+++ src/main/java/com/spring/transaction/entity/UserEntity.java
@@ -0,0 +1,4 @@
+package com.spring.transaction.entity;
+
+public class UserEntity {
+}
--- src/test/java/com/spring/transaction/SpringBootDailyPracticesApplicationTests.java
+++ src/test/java/com/spring/transaction/SpringBootDailyPracticesApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spring.transaction;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
